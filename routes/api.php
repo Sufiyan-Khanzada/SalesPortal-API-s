f954eb29a2fd7977ec95bf9eb8d6f3673aa2bfb1
Sufiyan-Khanzada <sufiyankhanzada1254@gmail.com>
2023-04-08 01:19:12 +0500

    All API's

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PassportController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('register', [PassportController::class, 'register']);
+Route::post('login', [PassportController::class, 'login']);
+
+// put all api protected routes here
+Route::middleware('auth:api')->group(function () {
+    Route::post('user-detail', [PassportController::class, 'userDetail']);
+    Route::post('logout', [PassportController::class, 'logout']);
+});
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
